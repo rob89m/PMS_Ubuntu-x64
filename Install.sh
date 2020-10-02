251682d4360b8eceb76d7c05a0128ce05354b281
rob89m <rob89m@outlook.com>
2020-10-02 13:13:51 +1000

    Create Install.sh

---
+++ Install.sh
@@ -0,0 +1,3 @@
+wget https://downloads.plex.tv/plex-media-server-new/1.20.1.3252-a78fef9a9/debian/plexmediaserver_1.20.1.3252-a78fef9a9_amd64.deb /tmp/plexmediaserver_1.20.1.3252-a78fef9a9_amd64.deb
+
+sudo dpkg -i /tmp/plexmediaserver_1.20.1.3252-a78fef9a9_amd64.deb
